e236128d9cc4eece10c188ef718feda2e79d3066
maciej.ladanek <maciej.ladanek@home.net.pl>
2021-01-26 21:37:30 +0100

    initial

---
+++ precache-manifest.77004afb88ea4fd28bbb9b31ebbd396d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "824fecd8011697a39ef3",
+    "url": "/static/css/main.a71559b7.chunk.css"
+  },
+  {
+    "revision": "824fecd8011697a39ef3",
+    "url": "/static/js/main.824fecd8.chunk.js"
+  },
+  {
+    "revision": "88c707d9ad64dd6278e6",
+    "url": "/static/css/1.11896431.chunk.css"
+  },
+  {
+    "revision": "88c707d9ad64dd6278e6",
+    "url": "/static/js/1.88c707d9.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "8c3aeb2dc64e91db979f17469c90785f",
+    "url": "/index.html"
+  }
+];
